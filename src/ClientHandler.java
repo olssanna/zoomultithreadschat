85889c91f1877d0dfea528e50eea37e26e77a882
olssanna123 <57717845+olssanna123@users.noreply.github.com>
2023-09-06 20:32:28 +0200

    Initial commit

--- src/ClientHandler.java
+++ src/ClientHandler.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class ClientHandler {
+import java.io.*;
+import java.net.Socket;
+import java.util.ArrayList;
+public class ClientHandler implements Runnable{
+    public static ArrayList<ClientHandler> clientHandlers = new ArrayList();
+    private Socket socket;
+    private BufferedReader bufferedReader;
+    private BufferedWriter bufferedWriter;
+    private String clientUsername;
+
+    public ClientHandler(Socket socket) {
+        try {
+            this.socket = socket;
+            this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            this.clientUsername = bufferedReader.readLine();
+            clientHandlers.add(this);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void run() {
+
+    }
 }
